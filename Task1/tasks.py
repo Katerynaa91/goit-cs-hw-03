90ba6f2c7914b06193c17f0d12c96246c5125e6d
Katerynaa91 <157005316+Katerynaa91@users.noreply.github.com>
2024-11-24 18:32:47 +0200

    Additional file containing data for tasks table

--- Task1/tasks.py
+++ Task1/tasks.py
@@ -1,1 +1,19 @@
+# Список даних для таблиці tasks
 
+TASKS = [('Code Review', 'Review a teammate\'s recent code submission and provide constructive feedback'), 
+          ('Team Stand-up', 'Organize a daily stand-up meeting to discuss progress and roadblocks'), 
+          ('Tech Blog Post', 'Write a blog post about a recent technology trend relevant to your team\'s work'), 
+          ('Security Training', 'Organize a training session on cybersecurity best practices for your team'),
+          ('Client Meeting Preparation', 'Prepare an agenda and presentation for an upcoming client meeting'),
+          ('System Upgrade', 'Plan and execute an upgrade for a software application used by your team'),
+          ('Network Security Audit', 'Conduct a basic audit of your team\'s network security measures'),
+          ('Mentorship', 'Pair up with a junior developer for a mentoring session to share knowledge and best practices'),
+          ('Project Documentation', 'Update the project documentation to reflect recent changes and improvements'),
+          ('Automation Script', 'Write a simple automation script to streamline a repetitive task'),
+          ('Networking Event', 'Attend a virtual networking event or meetup to connect with industry professionals'),
+          ('Team Building Activity', 'Organize a virtual team-building activity to boost morale and collaboration'),
+          ('System Monitoring', 'Set up or review system monitoring tools to ensure optimal performance and uptime'),
+          ('Create a Backup Strategy', None),
+          ('Implement Security Protocols', None)
+        ]
+          
